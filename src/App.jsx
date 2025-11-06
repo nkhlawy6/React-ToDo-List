ddce6d5c861573f96053cfd095220a48132d419d
emauser1 <emauser1@mci.ext>
2025-11-06 22:20:04 +0200

    Todo List: Design the overall structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
- 
- 
- 
-import './App.css'
-import ToDoList from './components/ToDolist'
+import { Box } from "@mui/material";
+import ToDoList from "./components/ToDolist";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 function App() {
-  
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#113A5D",
+      },
+      secondary: {
+        main: "#EFE9E3",
+      },
+      tertiary: {
+        main: "#C9B59C",
+      },
+    },
+  });
 
   return (
-    <>
-    <ToDoList/>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <Box
+        className=""
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "primary.main",
+          width: "100vw",
+          height: "100vh",
+        }}
+      >
+        <ToDoList />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/ToDolist.jsx
+++ src/components/ToDolist.jsx
@@ -1,7 +1,56 @@
- 
-
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Divider } from "@mui/material";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 export default function ToDoList() {
+  let bull = "mahmoud";
   return (
-  <h1>ToDo List</h1>
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm" sx={{ bgcolor: "secondary.main",padding:'20px' }}>
+        {/* main heading */}
+           {/* "Remove the background from the main title here after picking its color, and use that color as the background for each added task." */}
+        <Card sx={{ minWidth: 275, }}>
+          <CardContent sx={{textAlign: "center"}}>
+            <Typography
+              gutterBottom
+              sx={{ color: "tertiary.main" }}
+              variant={'h1'}
+            >
+              ToDo List
+            </Typography>
+            <Divider />
+            <ToggleButtonGroup
+              // value={alignment}
+              exclusive
+              // onChange={handleAlignment}
+              aria-label="text alignment"
+              sx={{mt:'25px'}}
+            >
+              <ToggleButton value="left" aria-label="left aligned"      sx={{}}>
+                All
+              </ToggleButton>
+              <ToggleButton value="center" aria-label="centered">
+                Pending
+              </ToggleButton>
+              <ToggleButton value="right" aria-label="right aligned">
+                done
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </CardContent>
+          <CardActions sx={{}}>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+        {/* ---------- End main heading ------------*/}
+      </Container>
+    </>
   );
 }
